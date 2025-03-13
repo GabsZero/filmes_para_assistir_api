09b64efba4b73cccc81236a695d20cf7b3f23abc
João de Souza <joaogbdesouza@gmail.com>
2025-03-12 22:21:56 -0300

    removendo migrate

--- cmd/database/database.go
+++ cmd/database/database.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 
-	"github.com/golang-migrate/migrate"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -13,15 +12,6 @@ func InitDB() *Queries {
 
 	// só pra fazer funcionar durante desenvolvimento
 	conn, err := pgx.Connect(ctx, "user=root password=postgres dbname=assistir_filmes_api host=filmes_para_assistir_db port=5432 sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	m, err := migrate.New(
-		"file://cmd/database/migrations",
-		"postgres://filmes_para_assistir_db:5432/assistir_filmes_api?sslmode=enable")
-	m.Steps(2)
-
 	if err != nil {
 		log.Fatal(err)
 	}
